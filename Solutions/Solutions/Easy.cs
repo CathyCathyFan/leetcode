1a2bcd1c5c939f2ee4e4187eb20c479093609a5d
Baomei Fan (PACTERA TECHNOLOGIES INC) <v-bafa@microsoft.com>
2019-08-10 22:06:03 -0700

    Finish Add Two Number

--- Solutions/Solutions/Easy.cs
+++ Solutions/Solutions/Easy.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Solutions
 {
-    [TestClass]
+    
     public class Easy
     {
-        [TestMethod]
+        public static void Main()
+        {
+            Console.Read();
+        }
+       
         public int[] TwoSum()
         {
             //Case 1
@@ -55,7 +60,7 @@ namespace Solutions
             //}
             #endregion
 
-            #region //Solution 3 Hash Table
+            #region //Solution 3 Dictionary
             //Dictionary<int, int> ht = new Dictionary<int, int>();
 
             //for (int i = 0; i < nums.Length; i++)
@@ -78,5 +83,55 @@ namespace Solutions
 
             return null;
         }
+
+        
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            if (l1 == null || l2 == null)
+            {
+                return l1 == null ? l2 : l1;
+            }
+
+            ListNode l3 = new ListNode(0);
+
+            ListNode l = l3;
+            int over = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                int val1 = l1 == null ? 0 : l1.val;
+                int val2 = l2 == null ? 0 : l2.val;
+
+                l3.val = (val1 + val2 + over) % 10;
+                over = (val1 + val2 + over) / 10;
+
+                l1 = l1 != null && l1.next != null ? l1.next : null;
+                l2 = l2 != null && l2.next != null ? l2.next : null;
+
+                if (l1 != null || l2 != null || over > 0)
+                {
+                    l3.next = new ListNode(over);
+                    l3 = l3.next;
+                }
+            }
+
+            return l;
+        }
+
+        
+        public int LengthOfLongestSubstring()
+        {
+           string s = "pwwkew";
+            if (s == null || s.Length == 0)  return 0;
+
+            return count;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
     }
 }
